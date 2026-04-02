6e820dc683aea9bb3b63f4ee3e8e90d2859d598c
Ivan <navigatorprestigio7777@gmail.com>
2026-04-02 22:38:01 +0300

    made insert without recursion

--- include/trees.hpp
+++ include/trees.hpp
@@ -15,10 +15,10 @@ class SearchTree;
 
 template <typename KeyT>
 int range_query(const SearchTree<KeyT>& s, const KeyT& fst, const KeyT& snd) {
-  if (!s.top) return 0;
+  if (!s.top_) return 0;
   int result = 0;
   std::vector<const typename SearchTree<KeyT>::Node*> stack;
-  stack.push_back(s.top);
+  stack.push_back(s.top_);
   while (!stack.empty()) {
     const typename SearchTree<KeyT>::Node* current = stack.back();
     stack.pop_back();
@@ -34,12 +34,12 @@ int range_query(const SearchTree<KeyT>& s, const KeyT& fst, const KeyT& snd) {
 template <typename KeyT>
 std::ostream& operator<<(std::ostream& os,
                          const Trees::SearchTree<KeyT>& tree) {
-  int rows = tree.top->height_ + 1;
-  int cols = std::pow(2, tree.top->height_ + 1) - 1;
+  int rows = tree.top_->height_ + 1;
+  int cols = std::pow(2, tree.top_->height_ + 1) - 1;
   std::vector<std::vector<std::string>> ans(rows,
                                             std::vector<std::string>(cols, ""));
 
-  tree.inorder(tree.top, 0, (cols - 1) / 2, ans);
+  tree.inorder(tree.top_, 0, (cols - 1) / 2, ans);
 
   for (auto& row : ans) {
     for (auto& cell : row) {
@@ -55,9 +55,11 @@ std::ostream& operator<<(std::ostream& os,
 
 template <typename KeyT>
 class SearchTree {
+  enum Node_color { Red, Black };
   struct Node {
     KeyT key_;
-    Node *left_ = nullptr, *right_ = nullptr;
+    Node *parent_ = nullptr, *left_ = nullptr, *right_ = nullptr;
+    Node_color color_ = Red;
     int height_ = 0;
     int get_height() const {
       if (!left_ && !right_)
@@ -71,10 +73,31 @@ class SearchTree {
     }
     Node(KeyT key) : key_(key) {};
   };
-  Node* top = nullptr;
+  Node* top_ = nullptr;
 
  public:
-  void insert(const KeyT& key) { top = recur_insert(top, key); }
+  void insert(const KeyT& key) {
+    Node* z = new Node{key};
+    Node* y = nullptr;
+    Node* x = top_;
+    while (x) {
+      y = x;
+      if (z->key_ < x->key_)
+        x = x->left_;
+      else if (z->key_ > x->key_)
+        x = x->right_;
+      else
+        return;
+    }
+    z->parent_ = y;
+    if (!y)
+      top_ = z;
+    else if (z->key_ < y->key_)
+      y->left_ = z;
+    else
+      y->right_ = z;
+    insert_fixup(z);
+  }
 
   friend int range_query<KeyT>(const SearchTree<KeyT>& s, const KeyT& fst,
                                const KeyT& snd);
@@ -82,112 +105,112 @@ class SearchTree {
                                          const Trees::SearchTree<KeyT>& tree);
 
  private:
-  Node* recur_insert(Node* node, const KeyT& key) {
-    if (node == nullptr) return new Node{key};
-    if (key < node->key_)
-      node->left_ = recur_insert(node->left_, key);
-    else if (key > node->key_)
-      node->right_ = recur_insert(node->right_, key);
-    else  // Equal keys are not allowed
-      return node;
+  void insert_fixup(Node* z) {
+    while (z->parent_ && z->parent_->color_ == Red) {
+      if (z->parent_ == z->parent_->parent_->left_) {
+        // z->parent_->parent_ exists because if z->parent_ is top_, then
+        // z->parent_ is black. Since we enter a loop iteration only if
+        // z->parent_ is red, we know that z->parent_ can not be top_. hence
+        // z->parent_->parent_exists
 
-    node->height_ = node->get_height();
-
-    // balance factor of the node
-    int balance;
-
-    if (!node->left_ && !node->right_)
-      balance = 0;
-    else if (!node->left_ && node->right_)
-      balance = -1 - node->right_->height_;
-    else if (node->left_ && !node->right_)
-      balance = node->left_->height_ - -1;
-    else
-      balance = node->left_->height_ - node->right_->height_;
-
-    try {
-      // Left left
-      if (balance > 1 && key < node->left_->key_) return rightRotate(node);
-    } catch (const std::logic_error& e) {
-      std::cerr << "Invalid left left rotation" << e.what() << ' ' << key
-                << std::endl;
-      throw std::exception{};
-    }
-    try {
-      // Right right
-      if (balance < -1 && key > node->right_->key_) return leftRotate(node);
-    } catch (const std::logic_error& e) {
-      std::cerr << "Invalid Right right rotation" << e.what() << ' ' << key
-                << std::endl;
-      throw std::exception{};
-    }
-    // Left right
-    try {
-      if (balance > 1 && key > node->left_->key_) {
-        node->left_ = leftRotate(node->left_);
-        return rightRotate(node);
+        // y - uncle of z
+        Node* y = z->parent_->parent_->right_;
+        // case 1:
+        if (y && y->color_ == Red) {
+          z->parent_->color_ = Black;
+          y->color_ = Black;
+          z->parent_->parent_->color_ = Red;
+          z = z->parent_->parent_;
+        } else {
+          // case 2:
+          if (z == z->parent_->right_) {
+            z = z->parent_;
+            leftRotate(z);
+            // turned case 2 into case 3
+          }
+          // case 3:
+          z->parent_->color_ = Black;
+          z->parent_->parent_->color_ = Red;
+          rightRotate(z->parent_->parent_);
+        }
+      } else {
+        Node* y = z->parent_->parent_->left_;
+        // case 1:
+        if (y && y->color_ == Red) {
+          z->parent_->color_ = Black;
+          y->color_ = Black;
+          z->parent_->parent_->color_ = Red;
+          z = z->parent_->parent_;
+        } else {
+          // case 2:
+          if (z == z->parent_->left_) {
+            z = z->parent_;
+            rightRotate(z);
+            // turned case 2 into case 3
+          }
+          // case 3:
+          z->parent_->color_ = Black;
+          z->parent_->parent_->color_ = Red;
+          leftRotate(z->parent_->parent_);
+        }
       }
-    } catch (const std::logic_error& e) {
-      std::cerr << std::endl
-                << "Invalid left right rotation" << e.what() << ' ' << key
-                << std::endl;
-      throw std::exception{};
     }
-    try {
-      // Right left
-      if (balance < -1 && key < node->right_->key_) {
-        node->right_ = rightRotate(node->right_);
-        return leftRotate(node);
-      }
-    } catch (const std::logic_error& e) {
-      std::cerr << std::endl
-                << "Invalid right left rotation of " << node->key_ << ": "
-                << e.what() << " Tried to insert key " << key << std::endl;
-      throw std::exception{};
-    }
-    return node;
+    top_->color_ = Black;
   }
-
-  Node* rightRotate(Node* y) {
-    if (!y) return y;
+  void rightRotate(Node* y) {
+    if (!y) return;
     Node* x = y->left_;
     if (!x)
       throw std::logic_error(
-          "Unexpected null node in tree rightRotation. The node is " +
+          "Unexpected null left node in tree rightRotation. The node is " +
           std::to_string(y->key_));
 
     // Rotation
     y->left_ = x->right_;
+    if (x->right_) x->right_->parent_ = y;
+    x->parent_ = y->parent_;
+    if (!y->parent_)
+      top_ = x;
+    else if (y == y->parent_->right_)
+      y->parent_->right_ = x;
+    else
+      y->parent_->left_ = x;
     x->right_ = y;
+    y->parent_ = x;
 
     y->height_ = y->get_height();
     x->height_ = x->get_height();
-
-    return x;
   }
 
-  Node* leftRotate(Node* x) {
-    if (!x) return x;
+  void leftRotate(Node* x) {
+    if (!x) return;
     Node* y = x->right_;
     if (!y)
       throw std::logic_error(
-          "Unexpected null node in tree leftRotation. The node is " +
+          "Unexpected null right node in tree leftRotation. The node is " +
           std::to_string(x->key_));
 
     // Rotation
     x->right_ = y->left_;
+    if (y->left_) y->left_->parent_ = x;
+    y->parent_ = x->parent_;
+    if (!x->parent_)
+      top_ = y;
+    else if (x == x->parent_->left_)
+      x->parent_->left_ = y;
+    else
+      x->parent_->right_ = y;
     y->left_ = x;
+    x->parent_ = y;
 
     x->height_ = x->get_height();
     y->height_ = y->get_height();
-
-    return y;
   }
 
   void inorder(const Node* node, const int& row, const int& col,
                std::vector<std::vector<std::string>>& ans) const {
     if (!node) return;
-    int offset = std::pow(2, top->height_ - row - 1);
+    int offset = std::pow(2, top_->height_ - row - 1);
     if (node->left_) inorder(node->left_, row + 1, col - offset, ans);
     ans[row][col] = std::to_string(node->key_);
     if (node->right_) inorder(node->right_, row + 1, col + offset, ans);
